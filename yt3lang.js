905dc7b169fe59108a2d3dac07695537b273250f
iHussam <istoretech3@gmail.com>
2018-06-21 18:37:44 +0100

    Create yt3lang.js

---
+++ yt3lang.js
@@ -0,0 +1,2 @@
+var ads = true;
+$(document).ready(function(){$("#nav_languages").click(function(){return"none"==$("#languages").css("display")?$("#languages").slideDown():$("#languages").slideUp(),!1})});
